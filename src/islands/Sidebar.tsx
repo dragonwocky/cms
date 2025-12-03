be7e71ca276a79fa3fdebd9072b0bb04d7b492e4
dragonwocky <thedragonring.bod@gmail.com>
2025-12-03 21:10:42 +1100

    feat: icon-only sidebar on mobile

--- src/islands/Sidebar.tsx
+++ src/islands/Sidebar.tsx
@@ -20,19 +20,20 @@ const Sidebar = () => {
   return (
     <aside
       classList={{
-        "w-xs px-2 mr-8 text-sm flex flex-col gap-px": true,
+        "text-sm flex flex-col gap-px": true,
         "*:w-full *:h-8 *:flex *:items-center *:gap-2 *:select-none *:rounded-md *:font-medium":
           true,
         "**:[svg]:h-5 **:[svg]:w-5 **:[svg]:text-neutral-500 dark:**:[svg]:text-neutral-400":
           true,
+        "w-16 mr-2 md:w-xs md:px-2 md:mr-8 **:[span]:hidden md:**:[span,h2]:inline": true
       }}
     >
-      <h1 class="text-sky-600 dark:text-sky-400">
+      <Link href="#">
         <Construction />
-        <span>Site Dashboard</span>
-      </h1>
+        <span>Under Construction</span>
+      </Link>
 
-      <Title>Collections</Title>
+      <Title><span>Collections</span></Title>
       <Link href="#" active>
         <Library />
         <span>Pages</span>
@@ -50,7 +51,7 @@ const Sidebar = () => {
         <span>Media</span>
       </Link>
 
-      <Title>Settings</Title>
+      <Title><span>Settings</span></Title>
       <Link href="#">
         <Signature />
         <span>Metadata</span>
